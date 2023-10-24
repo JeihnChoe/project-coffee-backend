d61e2d8c0019ed5450903bd62c2b6bdb7bb815f6
jwy4285 <jwy4285@naver.com>
2023-10-24 10:25:36 +0900

    비밀번호변경

--- src/main/java/shop/mtcoding/projectcoffeebackend/user/UserJPARepository.java
+++ src/main/java/shop/mtcoding/projectcoffeebackend/user/UserJPARepository.java
@@ -1,9 +1,13 @@
 package shop.mtcoding.projectcoffeebackend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserJPARepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
+
+    // @Query(value = "select u from User u where email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
