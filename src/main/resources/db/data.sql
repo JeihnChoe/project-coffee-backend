713630345ee7b57ef491eceaf27329b983a7ded2
JeihnChoe <baronjae@gmail.com>
2023-10-24 17:29:19 +0900

    오류수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user_tb (`id`,`email`,`password`,`username`,`phonenumber`,`manager`,`created_at`,`updated_at`) VALUES ('1', 'ssar@nate.com', 'a12345678!', 'ssar','01022222222',1,null,null); -- ssar 유저 id 임
-INSERT INTO user_tb (`id`,`email`,`password`,`username`,`phonenumber`,`manager`,`created_at`,`updated_at`) VALUES ('2', 'cos@nate.com', 'a12345678!', 'cos','01022221122',2,null,null); -- cos 관리자 id 임
+INSERT INTO user_tb (`id`,`email`,`password`,`username`,`phonenumber`,`manager`,`created_at`,`updated_at`) VALUES ('1', 'ssar@nate.com', 'a12345678!', 'ssar','01022222222',1,'2023-10-24 14:20:36.077','2023-10-24 14:20:36.077'); -- ssar 유저 id 임
+INSERT INTO user_tb (`id`,`email`,`password`,`username`,`phonenumber`,`manager`,`created_at`,`updated_at`) VALUES ('2', 'cos@nate.com', 'a12345678!', 'cos','01022221122',2,'2023-10-24 14:20:36.077','2023-10-24 14:20:36.077'); -- cos 관리자 id 임
 
 
         -- this.id = id;
