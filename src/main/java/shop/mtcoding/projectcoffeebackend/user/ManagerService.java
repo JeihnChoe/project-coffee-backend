c64192ccaa18c59e262855ffdc31f730940b5e2f
josohyun <hyun980110@naver.com>
2023-10-31 17:48:42 +0900

    관리자 음료 등록 하는중

--- src/main/java/shop/mtcoding/projectcoffeebackend/user/ManagerService.java
+++ src/main/java/shop/mtcoding/projectcoffeebackend/user/ManagerService.java
@@ -1,5 +1,18 @@
 package shop.mtcoding.projectcoffeebackend.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import shop.mtcoding.projectcoffeebackend.beverage.BeverageJPARepository;
+
+@Service
 public class ManagerService {
+
+    @Autowired
+    BeverageJPARepository beverageJPARepository;
+
+    public void 음료추가() {
+        beverageJPARepository.save(null);
+    }
 
 }
