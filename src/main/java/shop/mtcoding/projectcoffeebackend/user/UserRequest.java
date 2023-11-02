d8609ad9e3206c27fca38882683da0816bbb9135
JeihnChoe <baronjae@gmail.com>
2023-11-02 17:52:13 +0900

    오늘한게없어요

--- src/main/java/shop/mtcoding/projectcoffeebackend/user/UserRequest.java
+++ src/main/java/shop/mtcoding/projectcoffeebackend/user/UserRequest.java
@@ -14,7 +14,6 @@ public class UserRequest {
     public static class JoinDTO {
 
         @NotEmpty
-        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일 형식으로 작성해주세요")
         private String loginId;
 
         @NotEmpty
