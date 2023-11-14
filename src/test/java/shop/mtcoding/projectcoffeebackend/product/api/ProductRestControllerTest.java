14c556f0ccef9974b0fd345afdae0ec90ad53ac3
JeihnChoe <baronjae@gmail.com>
2023-11-14 17:18:11 +0900

    프로모션 테스트코드 작성 완료

--- src/test/java/shop/mtcoding/projectcoffeebackend/product/api/ProductRestControllerTest.java
+++ src/test/java/shop/mtcoding/projectcoffeebackend/product/api/ProductRestControllerTest.java
@@ -1,4 +1,4 @@
-package shop.mtcoding.projectcoffeebackend.promotion.api;
+package shop.mtcoding.projectcoffeebackend.product.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,38 +10,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import shop.mtcoding.projectcoffeebackend.MyWithRestDoc;
 
 @SpringBootTest
-public class PromotionRestControllerTest extends MyWithRestDoc {
+public class ProductRestControllerTest extends MyWithRestDoc {
 
     @Test
-    public void viewPromotionList_test() throws Exception {
+    public void viewProductListByCategoryId() throws Exception {
 
         //when
         ResultActions resultActions =
                 mockMvc.perform(
-                        MockMvcRequestBuilders.get("/api/promotion/viewpromotionlist")
+                        MockMvcRequestBuilders.get("/api/product/1/productlist")
                                 .contentType(MediaType.APPLICATION_JSON)
                 );
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println(responseBody);
-        //then
-        resultActions
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.error").isEmpty())
-                .andDo(MockMvcResultHandlers.print())
-                .andDo(document);
-    }
 
-    @Test
-    public void detailPromotion_test() throws Exception {
-        //when
-        ResultActions resultActions =
-                mockMvc.perform(
-                        MockMvcRequestBuilders.get("/api/promotion/1/detailpage")
-                                .contentType(MediaType.APPLICATION_JSON)
-                );
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        System.out.println(responseBody);
         //then
         resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -50,7 +32,8 @@ public class PromotionRestControllerTest extends MyWithRestDoc {
                 .andDo(MockMvcResultHandlers.print())
                 .andDo(document);
 
+
     }
+
 
 }
-
